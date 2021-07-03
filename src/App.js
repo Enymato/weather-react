ace5001eae35946cd8e58eb6d4c61a07eadfa1dd
Enymato <79268301+Enymato@users.noreply.github.com>
2021-07-03 10:37:52 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import SearchWeather from "./SearchWeather.js";
 function App() {
   return (
     <div className="App">
+      <div className="container">
+
       <h1 className="title"> The Weather </h1>
       
       <SearchWeather/> 
+    </div>
     </div>
   );
 }
